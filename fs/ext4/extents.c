3380b61a1202d3fe823a93739f72bacafe79a08d
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2025-12-21 14:21:04 +0530

    ext4: move zeroout to serparate function (SQUASH)

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3312,6 +3312,115 @@ out_path:
 	return path;
 }
 
+static struct ext4_ext_path *
+ext4_split_extent_zeroout(handle_t *handle, struct inode *inode,
+			  struct ext4_ext_path *path,
+			  struct ext4_map_blocks *map, int flags)
+{
+	struct ext4_extent *ex;
+	unsigned int ee_len, depth;
+	ext4_lblk_t ee_block;
+	uint64_t lblk, pblk, len;
+	int is_unwrit;
+	int err = 0;
+
+	depth = ext_depth(inode);
+	ex = path[depth].p_ext;
+	ee_block = le32_to_cpu(ex->ee_block);
+	ee_len = ext4_ext_get_actual_len(ex);
+	is_unwrit = ext4_ext_is_unwritten(ex);
+
+	if (flags & EXT4_GET_BLOCKS_CONVERT) {
+		/*
+		 * EXT4_GET_BLOCKS_CONVERT: Caller wants the range specified by
+		 * map to be initialized. Zeroout everything everything except
+		 * the map range.
+		 */
+
+		loff_t map_end = (loff_t) map->m_lblk + map->m_len;
+		loff_t ex_end = (loff_t) ee_block + ee_len;
+
+		if (!is_unwrit)
+			/* Shouldn't happen. Just exit */
+			return ERR_PTR(-EINVAL);
+
+		/* zeroout left */
+		if (map->m_lblk > ee_block) {
+			lblk = ee_block;
+			len = map->m_lblk - ee_block;
+			pblk = ext4_ext_pblock(ex);
+			err = ext4_issue_zeroout(inode, lblk, pblk, len);
+			if (err)
+				/* ZEROOUT failed, just return original error */
+				return ERR_PTR(err);
+		}
+
+		/* zeroout right */
+		if (map->m_lblk + map->m_len < ee_block + ee_len) {
+			lblk = map_end;
+			len = ex_end - map_end;
+			pblk = ext4_ext_pblock(ex) + (map_end - ee_block);
+			err = ext4_issue_zeroout(inode, lblk, pblk, len);
+			if (err)
+				/* ZEROOUT failed, just return original error */
+				return ERR_PTR(err);
+		}
+	} else if (flags & EXT4_GET_BLOCKS_UNWRIT_EXT) {
+		/*
+		 * EXT4_GET_BLOCKS_UNWRIT_EXT: Today, this flag
+		 * implicitly implies that callers when wanting an
+		 * unwritten to unwritten split. So zeroout the whole
+		 * extent.
+		 *
+		 * TODO: The implicit meaning of the flag is not ideal
+		 * and eventually we should aim for a more well defined
+		 * behavior
+		 */
+
+		if (!is_unwrit)
+			/* Shouldn't happen. Just exit */
+			return ERR_PTR(-EINVAL);
+
+		lblk = ee_block;
+		len = ee_len;
+		pblk = ext4_ext_pblock(ex);
+		err = ext4_issue_zeroout(inode, lblk, pblk, len);
+		if (err)
+			/* ZEROOUT failed, just return original error */
+			return ERR_PTR(err);
+	} else if (flags & EXT4_GET_BLOCKS_CONVERT_UNWRITTEN) {
+		/*
+		 * EXT4_GET_BLOCKS_CONVERT_UNWRITTEN: Caller wants the
+		 * range specified by map to be marked unwritten.
+		 * Zeroout the map range leaving rest as it is.
+		 */
+
+		if (is_unwrit)
+			/* Shouldn't happen. Just exit */
+			return ERR_PTR(-EINVAL);
+
+		lblk = map->m_lblk;
+		len = map->m_len;
+		pblk = ext4_ext_pblock(ex) + (map->m_lblk - ee_block);
+		err = ext4_issue_zeroout(inode, lblk, pblk, len);
+		if (err)
+			/* ZEROOUT failed, just return original error */
+			return ERR_PTR(err);
+	}
+
+	err = ext4_ext_get_access(handle, inode, path + depth);
+	if (err)
+		return ERR_PTR(err);
+
+	ex->ee_len = cpu_to_le16(ee_len);
+
+	ext4_ext_dirty(handle, inode, path + path->p_depth);
+	if (err)
+		return ERR_PTR(err);
+
+	return 0;
+}
+
 /*
  * ext4_split_extent() splits an extent and mark extent which is covered
  * by @map as split_flags indicates
@@ -3330,10 +3439,10 @@ static struct ext4_ext_path *ext4_split_extent(handle_t *handle,
 					       int split_flag, int flags,
 					       unsigned int *allocated)
 {
-	ext4_lblk_t ee_block;
+	ext4_lblk_t ee_block, orig_ee_block;
 	struct ext4_extent *ex;
-	unsigned int ee_len, depth;
-	int unwritten;
+	unsigned int ee_len, orig_ee_len, depth;
+	int unwritten, orig_unwritten;
 	int split_flag1 = 0, flags1 = 0;
 	int err = 0, orig_err;
 
@@ -3342,6 +3451,10 @@ static struct ext4_ext_path *ext4_split_extent(handle_t *handle,
 	ee_block = le32_to_cpu(ex->ee_block);
 	ee_len = ext4_ext_get_actual_len(ex);
 	unwritten = ext4_ext_is_unwritten(ex);
+
+	orig_ee_block = ee_block;
+	orig_ee_len = ee_len;
+	orig_unwritten = unwritten;
 
 	/* Do not cache extents that are in the process of being modified. */
 	flags |= EXT4_EX_NOCACHE;
@@ -3408,124 +3521,34 @@ try_zeroout:
 	 * to initialize as a last resort
 	 */
 	if (split_flag & EXT4_EXT_MAY_ZEROOUT) {
-		uint64_t lblk, pblk, len;
-		unsigned int orig_ee_block = ee_block, orig_ee_len = ee_len;
-		int is_unwrit;
-		int err = 0;
-
 		path = ext4_find_extent(inode, map->m_lblk, NULL, flags);
 		if (IS_ERR(path))
 			return path;
+
 		depth = ext_depth(inode);
 		ex = path[depth].p_ext;
 		ee_block = le32_to_cpu(ex->ee_block);
 		ee_len = ext4_ext_get_actual_len(ex);
-		is_unwrit = ext4_ext_is_unwritten(ex);
+		unwritten = ext4_ext_is_unwritten(ex);
 
-		if (WARN_ON((ee_block != orig_ee_block ||
-			     ee_len != orig_ee_len))) {
+		if (WARN_ON(ee_block != orig_ee_block ||
+			    ee_len != orig_ee_len ||
+			    unwritten != orig_unwritten)) {
 			/*
-			 * The extent to zerout should have been unchange
-			 * but its not, just return error to caller
-			 */
+			* The extent to zerout should have been unchange
+			* but its not, just return error to caller
+			*/
 			return ERR_PTR(orig_err);
 		}
 
-		if (flags & EXT4_GET_BLOCKS_CONVERT) {
+		if (ext4_split_extent_zeroout(handle, inode, path, map,
+					      flags)) {
 			/*
-			 * EXT4_GET_BLOCKS_CONVERT: Caller wants the range specified by
-			 * map to be initialized. Zeroout everything everything except
-			 * the map range.
+			 * Something went wrong in zeroout, just return the
+			 * original error
 			 */
-
-			loff_t map_end = (loff_t)map->m_lblk + map->m_len;
-			loff_t ex_end = (loff_t)ee_block + ee_len;
-
-			if (!is_unwrit)
-				/* Shouldn't happen. Just exit */
-				return ERR_PTR(orig_err);
-
-			/* zeroout left */
-			if (map->m_lblk > ee_block) {
-				lblk = ee_block;
-				len = map->m_lblk - ee_block;
-				pblk = ext4_ext_pblock(ex);
-				if (ext4_issue_zeroout(inode, lblk, pblk, len))
-					/* ZEROOUT failed, just return original error */
-					return ERR_PTR(orig_err);
-			}
-
-			/* zeroout right */
-			if (map->m_lblk + map->m_len < ee_block + ee_len) {
-				lblk = map_end;
-				len = ex_end - map_end;
-				pblk = ext4_ext_pblock(ex) +
-				       (map_end - ee_block);
-				if (ext4_issue_zeroout(inode, lblk, pblk, len))
-					/* ZEROOUT failed, just return original error */
-					return ERR_PTR(orig_err);
-			}
-		} else if (flags & EXT4_GET_BLOCKS_UNWRIT_EXT) {
-			/*
-			 * EXT4_GET_BLOCKS_UNWRIT_EXT: Today, this flag
-			 * implicitly implies that callers when wanting an
-			 * unwritten to unwritten split. So zeroout the whole
-			 * extent.
-			 *
-			 * TODO: THe implicit meaning of the flag is not ideal
-			 * and eventually we should aim for a more well defined
-			 * behavior
-			 */
-
-			if(!is_unwrit)
-				/* Shouldn't happen. Just exit */
-				return ERR_PTR(orig_err);
-
-			lblk = ee_block;
-			len = ee_len;
-			pblk = ext4_ext_pblock(ex);
-			if (ext4_issue_zeroout(inode, lblk, pblk, len))
-				/* ZEROOUT failed, just return original error */
-				return ERR_PTR(orig_err);
-		} else if (flags & EXT4_GET_BLOCKS_CONVERT_UNWRITTEN) {
-			/*
-			 * EXT4_GET_BLOCKS_CONVERT_UNWRITTEN: Caller wants the
-			 * range specified by map to be marked unwritten.
-			 * Zeroout the map range leaving rest as it is.
-			 */
-
-			if(is_unwrit)
-				/* Shouldn't happen. Just exit */
-				return ERR_PTR(orig_err);
-
-			lblk = map->m_lblk;
-			len = map->m_len;
-			pblk = ext4_ext_pblock(ex) + (map->m_lblk - ee_block);
-			if (ext4_issue_zeroout(inode, lblk, pblk, len))
-				/* ZEROOUT failed, just return original error */
-				return ERR_PTR(orig_err);
+			return ERR_PTR(orig_err);
 		}
-
-		/*
-		 * /\* Zeroout succeeded, mark the extent as initialized *\/
-		 * path = ext4_find_extent(inode, ee_block, NULL,
-		 * 			flags | EXT4_EX_NOFAIL);
-		 */
-		/*
-		 * if (IS_ERR(path))
-		 * 	    return path;
-		 */
-
-		err = ext4_ext_get_access(handle, inode, path + depth);
-		if (err)
-			return ERR_PTR(err);
-
-		ex->ee_len = cpu_to_le16(ee_len);
-
-		ext4_ext_dirty(handle, inode, path + path->p_depth);
-		if (err)
-			return ERR_PTR(err);
-
 	}
 
 	/* There's an error and we can't zeroout, just return the err */
