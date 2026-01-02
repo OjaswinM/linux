34d6ef7a2ba058e56eca472d86c3d9ce001b178d
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2026-01-02 17:53:24 +0530

    ext4: kunit tests for higher level extent manipulation functions
    Add more kunit tests to cover the high level caller
    ext4_map_create_blocks(). We pass flags in a manner that covers
    the below function:
    
    1. ext4_ext_handle_unwritten_extents()
      1.1 - Split/Convert unwritten extent to written in endio convtext.
      1.2 - Split/Convert unwritten extent to written in non endio context.
      1.3 - Zeroout tests for the above 2 cases
    2. convert_initialized_extent() - Convert written extent to unwritten
       during zero range
    
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -542,8 +542,8 @@ static int ext4_map_query_blocks_next_in_leaf(handle_t *handle,
 	return map->m_len;
 }
 
-static int ext4_map_query_blocks(handle_t *handle, struct inode *inode,
-				 struct ext4_map_blocks *map, int flags)
+int ext4_map_query_blocks(handle_t *handle, struct inode *inode,
+			  struct ext4_map_blocks *map, int flags)
 {
 	unsigned int status;
 	int retval;
@@ -589,7 +589,7 @@ out:
 	return retval;
 }
 
-static int ext4_map_create_blocks(handle_t *handle, struct inode *inode,
+int ext4_map_create_blocks(handle_t *handle, struct inode *inode,
 				  struct ext4_map_blocks *map, int flags)
 {
 	unsigned int status;
